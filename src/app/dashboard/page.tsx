fc7b7d2e5fe506e4ce6372c556e1f021cb43784b
Abhishek <abhishek.konnayil@gmail.com>
2025-05-01 20:54:39 +0530

    external images adding

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Image from "next/image";
-import NatureImage from "../../../public/freenaturestock-2268-768x1152.jpg"
 
 const Dashboard = () => {
   if (7 < 5) {
     throw new Error("Error in Dashboard please note");
   }
-  return <div><Image src={NatureImage} alt={"Nature image"}/></div>;
+  return (
+    <div>
+      <Image
+        src="https://m.media-amazon.com/images/I/71RWw4rR6LL.jpg"
+        alt={"Nature image"}
+        width={1920}
+        height={1080}
+      />
+    </div>
+  );
 };
 
 export default Dashboard;
