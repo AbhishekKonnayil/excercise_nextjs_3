14e7fc0bb385d8ed63a111ef95df1e7b9cef5b17
Abhishek <abhishek.konnayil@gmail.com>
2025-05-01 15:33:55 +0530

    custom error page managing

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="bg-gradient-to-b from-gray-500 to-indigo-400 h-screen flex justify-center items-center">
+      <div className="bg-gray-900 rounded-xl p-6">
+        <h1 className="text-5xl text-white font-bold ">404! The page does not exist</h1>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
