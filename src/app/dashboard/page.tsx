4007638d842dee3a0e770739a0581ff01b660078
Abhishek <abhishek.konnayil@gmail.com>
2025-05-01 20:13:32 +0530

    interbal images adding

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,12 @@
-import { error } from "console";
 import React from "react";
+import Image from "next/image";
+import NatureImage from "../../../public/freenaturestock-2268-768x1152.jpg"
 
 const Dashboard = () => {
-  if (4 < 5) {
+  if (7 < 5) {
     throw new Error("Error in Dashboard please note");
   }
-  return <div>Dashboard</div>;
+  return <div><Image src={NatureImage} alt={"Nature image"}/></div>;
 };
 
 export default Dashboard;
