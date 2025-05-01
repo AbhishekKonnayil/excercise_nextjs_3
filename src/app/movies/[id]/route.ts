078f6cee12e112ef4ed1d303292e5d9c591c207a
Abhishek <abhishek.konnayil@gmail.com>
2025-05-02 00:02:15 +0530

    patch method

--- src/app/movies/[id]/route.ts
+++ src/app/movies/[id]/route.ts
@@ -10,3 +10,25 @@ export async function GET(
     ? new Response(JSON.stringify(movie))
     : new Response("Movie not found", { status: 404 });
 }
+export async function PATCH(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  const movieId = +id;
+  const movie = movies.find((m) => m.id == movieId);
+  if (!movie) {
+    return new Response("Movie not found", { status: 404 });
+  }
+  try {
+    const updatedMovie = await req.json();
+    const index = movies.findIndex((m) => m.id == movieId);
+    if (!index) {
+      return new Response("Movie not found", { status: 404 });
+    }
+    movies[index] = { ...movie, ...updatedMovie };
+    return new Response(JSON.stringify(movies[index]));
+  } catch (error) {
+    return new Response(JSON.stringify({ error: "Failed to parse JSON" }));
+  }
+}
