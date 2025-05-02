9bd06dda1e9e18ad2c487af1335983087014ffbf
Abhishek <abhishek.konnayil@gmail.com>
2025-05-02 21:53:40 +0530

    adding google fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { Roboto_Mono } from "next/font/google";
+const coralPixels = Roboto_Mono({ subsets: ["latin"], weight: "500" });
 
 const Home = () => {
-  return <div>Home</div>;
+  return <div><h1>Home</h1><p className={`${coralPixels.className}`}>This is home page</p></div>;
 };
 
 export default Home;
